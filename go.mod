8da36b9df0b66468ebb85bf02beba79ea41129ab
Damian Akpan <damiakpan@gmail.com>
2025-12-05 17:58:35 +0100

    Update

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.3
 require (
 	github.com/AdaLogics/go-fuzz-headers v0.0.0-20240806141605-e8a1dd7889d6
 	github.com/docker/docker v28.5.2+incompatible
+	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/jackc/pgx/v5 v5.7.6
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/sync v0.13.0
@@ -16,6 +17,7 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
 )
--- services/db_net/db_host.go
+++ services/db_net/db_host.go
@@ -7,14 +7,14 @@ import (
 	"net"
 	"os"
 	"time"
-
-	"crypto/rand"
-	"math/big"
-
-	"github.com/docker/docker/pkg/namesgenerator"
 )
 
-var hostname = "example.com"
+var (
+	dbPort  = os.Getenv("DB_PORT")
+	dbHost  = os.Getenv("DB_HOSTNAME")
+	appHost = os.Getenv("APP_HOSTNAME")
+)
+
 var connections = make(map[string]net.Listener)
 
 func CreateConnection(subdomain, port string) error {
@@ -31,7 +31,6 @@ func CreateConnection(subdomain, port string) error {
 	connections[subdomain] = listener
 	log.Printf("Listening on port %s for subdomain %s\n", port, subdomain)
 
-	// var hasListener bool = false
 	var closeChan = make(chan bool)
 
 	go func() {
@@ -44,55 +43,27 @@ func CreateConnection(subdomain, port string) error {
 	for {
 		clientConn, _ := listener.Accept()
 
-		domain := subdomain + "." + hostname
-		clientHost, _, _ := net.SplitHostPort(clientConn.LocalAddr().String())
-		// if hasListener {
-		// 	log.Printf("Rejected connection from %s to %s: already has active connection\n", clientHost, domain)
-		// 	clientConn.Close()
-		// 	continue
-		// }
-
-		// hasListener = true
+		domain := subdomain + "." + appHost
+		clientHost, _, _ := net.SplitHostPort(clientConn.RemoteAddr().String())
 		log.Printf("New connection from %s to %s\n", clientHost, domain)
 		go handleConnection(clientConn, 5*time.Minute, closeChan)
 	}
 }
 
-type loggingConn struct {
-	net.Conn
-	r io.Writer
-	w io.Writer
-}
-
-func (c *loggingConn) Read(b []byte) (int, error) {
-	n, err := c.Conn.Read(b)
-	if n > 0 && c.r != nil {
-		_, _ = c.r.Write(b[:n])
-	}
-	return n, err
-}
-
-func (c *loggingConn) Write(b []byte) (int, error) {
-	n, err := c.Conn.Write(b)
-	if n > 0 && c.w != nil {
-		_, _ = c.w.Write(b[:n])
-	}
-	return n, err
-}
-
 func handleConnection(client net.Conn, duration time.Duration, closeChan chan bool) {
-	targetConn, err := net.Dial("tcp", "localhost:5432")
+	targetConn, err := net.Dial("tcp", dbHost+":"+dbPort)
 	if err != nil {
 		client.Close()
 		return
 	}
 
-	// Wrap targetConn so it's still a net.Conn but logs traffic to stdout
-	targetConn = &loggingConn{Conn: targetConn, r: os.Stdout, w: os.Stdout}
+	targetConn = &loggingConn{
+		logger: os.Stdout,
+		Conn:   targetConn,
+	}
 
 	defer client.Close()
 
-	// Set the "Kill Switch"
 	timer := time.AfterFunc(duration, func() {
 		log.Println("Time limit reached! Killing connection.")
 		client.Close()
@@ -101,33 +72,6 @@ func handleConnection(client net.Conn, duration time.Duration, closeChan chan bo
 	})
 	defer timer.Stop()
 
-	// Pipe data back and forth
 	go io.Copy(targetConn, client)
 	io.Copy(client, targetConn)
-}
-
-func generateCryptoRandInt(min, max int) (int, error) {
-	// The range is inclusive, so max - min + 1 possible values
-	rangeSize := big.NewInt(int64(max - min + 1))
-
-	// rand.Int generates a value [0, rangeSize)
-	n, err := rand.Int(rand.Reader, rangeSize)
-	if err != nil {
-		return 0, err
-	}
-
-	// Add the minimum value to the result
-	return int(n.Int64()) + min, nil
-}
-
-func GenerateSubdomainAndPort() (string, string, string) {
-	name := namesgenerator.GetRandomName(10)
-	port, err := generateCryptoRandInt(20000, 40000)
-	if err != nil {
-		return "", "0", ""
-	}
-
-	fullName := name + "." + hostname
-
-	return name, fmt.Sprintf("%d", port), fullName
 }
